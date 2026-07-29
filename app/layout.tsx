1da7a8f2ab63638c2002a69fe8c5e1b2dc4d4b7d
Ashwinjauhary <ashwin2431333@gmail.com>
2026-07-29 14:06:40 +0530

    fix: add scroll-smooth to html to fix Next.js warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
